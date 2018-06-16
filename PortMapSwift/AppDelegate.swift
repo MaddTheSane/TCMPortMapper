4be4fddb455a3ef910e949fa7e4431481cba434b
C.W. Betts <computers57@hotmail.com>
2018-06-16 02:12:37 -0600

    Initial work on Swift version. More to test how well TCMPortMapper integrates into Swift.

---
+++ PortMapSwift/AppDelegate.swift
@@ -0,0 +1,97 @@
+//
+//  AppDelegate.swift
+//  PortMapSwift
+//
+//  Created by C.W. Betts on 6/16/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	@IBAction open func togglePortMapper(_ aSender: Any?) {
+		
+	}
+	
+	
+	@IBAction open func refresh(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func addMapping(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func removeMapping(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func addMappingEndSheet(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func addMappingCancelSheet(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func choosePreset(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func showInstructionalPanel(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func endInstructionalSheet(_ aSender: Any?) {
+		
+	}
+	
+	
+	@IBAction open func gotoPortMapHomepage(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func gotoTCMPortMapperSources(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func reportABug(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func showReleaseNotes(_ aSender: Any?) {
+		
+	}
+	
+	
+	@IBAction open func showAbout(_ aSender: Any?) {
+		
+	}
+	
+	
+	@IBAction open func requestUPNPMappingTable(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func requestUPNPMappingTableRemoveMappings(_ aSender: Any?) {
+		
+	}
+	
+	@IBAction open func requestUPNPMappingTableOKSheet(_ aSender: Any?) {
+		
+	}
+
+}
+
---
+++ PortMapSwift/TCMPortMappingAdditions.swift
@@ -0,0 +1,64 @@
+//
+//  TCMPortMappingAdditions.swift
+//  PortMapSwift
+//
+//  Created by C.W. Betts on 6/16/18.
+//
+
+import Foundation
+import TCMPortMapper
+
+private let userInfoMapKey = "userInfo"
+private let privatePortMapKey = "privatePort"
+private let desiredPublicPortMapKey = "desiredPublicPort"
+private let transportProtocolMapKey = "transportProtocol"
+
+extension TCMPortMapping {
+	convenience init(dictionaryRepresentation dr: [String: Any]) {
+		var newLPort: UInt16 = 0
+		if let newLPort2 = dr[privatePortMapKey] as? UInt16 {
+			newLPort = newLPort2
+		} else if let newLPort3 = dr[privatePortMapKey] as? Int {
+			newLPort = UInt16(truncatingIfNeeded: newLPort3)
+		}
+		var newDPort: UInt16 = 0
+		if let newLPort2 = dr[desiredPublicPortMapKey] as? UInt16 {
+			newDPort = newLPort2
+		} else if let newLPort3 = dr[desiredPublicPortMapKey] as? Int {
+			newDPort = UInt16(truncatingIfNeeded: newLPort3)
+		}
+		var usrInfo = dr[userInfoMapKey]
+		if usrInfo is NSNull {
+			usrInfo = nil
+		}
+		let theProtocol: TCMPortMappingTransportProtocol? = {
+			guard let prot2 = dr[transportProtocolMapKey] else {
+				return nil
+			}
+			if let prot3 = prot2 as? TCMPortMappingTransportProtocol {
+				return prot3
+			}
+			if let prot3 = prot2 as? TCMPortMappingTransportProtocol.RawValue {
+				return TCMPortMappingTransportProtocol(rawValue: prot3)
+			}
+			if let prot3 = prot2 as? Int,
+				let prot4 = TCMPortMappingTransportProtocol.RawValue(exactly: prot3) {
+				return TCMPortMappingTransportProtocol(rawValue: prot4)
+			}
+			return nil
+		}()
+		
+		
+		self.init(localPort: newLPort, desiredExternalPort: newDPort, transportProtocol: .TCP, userInfo: usrInfo)
+		if let theProtocol = theProtocol {
+			transportProtocol = theProtocol
+		}
+	}
+	
+    var dictionaryRepresentation: [String: Any] {
+        return [userInfoMapKey: userInfo ?? NSNull(),
+                privatePortMapKey: localPort,
+				desiredPublicPortMapKey: desiredExternalPort,
+				transportProtocolMapKey: transportProtocol.rawValue]
+    }
+}
