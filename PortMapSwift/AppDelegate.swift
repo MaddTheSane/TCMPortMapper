2f082ae9559280a65a2d65624a830d5b012a68c4
C.W. Betts <computers57@hotmail.com>
2018-06-16 17:41:34 -0600

    More Swift porting work.

--- PortMapSwift/AppDelegate.swift
+++ PortMapSwift/AppDelegate.swift
@@ -73,6 +73,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		center.addObserver(self, selector: #selector(AppDelegate.portMapperExternalIPAddressDidChange(_:)), name: .TCMPortMapperExternalIPAddressDidChange, object: pm)
 		center.addObserver(self, selector: #selector(AppDelegate.portMapperWillSearchForRouter(_:)), name: .TCMPortMapperWillStartSearchForRouter, object: pm)
 		center.addObserver(self, selector: #selector(AppDelegate.portMapperDidFindRouter(_:)), name: .TCMPortMapperDidFinishSearchForRouter, object: pm)
+		center.addObserver(self, selector: #selector(AppDelegate.portMappingDidChangeMappingStatus(_:)), name: .TCMPortMappingDidChangeMappingStatus, object: pm)
 		center.addObserver(self, selector: #selector(AppDelegate.startProgressIndicator(_:)), name: .TCMPortMapperDidStartWork, object: pm)
 		center.addObserver(self, selector: #selector(AppDelegate.stopProgressIndicator(_:)), name: .TCMPortMapperDidFinishWork, object: pm)
 		
@@ -122,6 +123,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationWillTerminate(_ aNotification: Notification) {
 		writeMappingDefaults()
 		TCMPortMapper.shared.stopBlocking()
+	}
+	
+	func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+		window.orderFront(self)
+		return false
 	}
 
 	private func writeMappingDefaults() {
@@ -186,6 +192,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		return externalIPAddress
 	}
 	
+	@objc func portMappingDidChangeMappingStatus(_ aNotification: Notification) {
+		replacedReferenceStringTextField.stringValue = ValueTransformer(forName: replacedStringFromPortMappingReferenceString)?.transformedValue(mappingsArrayController.selectedObjects) as? String ?? ""
+	}
+
 	@objc func startProgressIndicator(_ aNotification: Notification) {
 		globalProgressIndicator.startAnimation(self)
 		showUPNPMappingTableButton.isEnabled = false
@@ -204,7 +214,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			localIPAddressTextField.stringValue = ""
 		}
 	}
-
 	
 	@IBAction open func togglePortMapper(_ aSender: NSButton?) {
 		if aSender?.state == .on {
@@ -232,31 +241,84 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 	
 	@IBAction open func addMapping(_ aSender: Any?) {
-		
+		window.beginSheet(addSheetPanel) { (resp) in
+			self.addSheetPanel.orderOut(self)
+		}
 	}
 	
 	@IBAction open func removeMapping(_ aSender: Any?) {
-		
+		if let mappings = mappingsArrayController.selectedObjects as? [TCMPortMapping] {
+			for mapping in mappings {
+				if (mapping.userInfo as! [String: Any])["active"] as! Bool {
+					TCMPortMapper.shared.removePortMapping(mapping)
+				}
+				mapping.removeObserver(self, forKeyPath: "userInfo.active")
+			}
+			mappingsArrayController.remove(contentsOf: mappingsArrayController.selectedObjects)
+		}
 	}
 	
 	@IBAction open func addMappingEndSheet(_ aSender: Any?) {
+		let userInfo2: [String: Any] = ["active": true, "mappingTitle": addDescriptionField.stringValue, "referenceString": addReferenceStringField.stringValue]
+		let userInfo = NSMutableDictionary(dictionary: userInfo2, copyItems: true)
+		let mapping = TCMPortMapping(localPort: UInt16(addLocalPortField.integerValue), desiredExternalPort: UInt16(addDesiredField.integerValue), transportProtocol: .TCP, userInfo: userInfo)
+		var transportProtocol = TCMPortMappingTransportProtocol()
+		if addProtocolTCPButton.state == .on {
+			transportProtocol.insert(.TCP)
+		}
+		if addProtocolUDPButton.state == .on {
+			transportProtocol.insert(.UDP)
+		}
 		
+		mapping.transportProtocol = transportProtocol
+		mapping.addObserver(self, forKeyPath: "userInfo.active", options: [], context: nil)
+		mappingsArrayController.addObject(mapping)
+		TCMPortMapper.shared.addPortMapping(mapping)
+		window.endSheet(addSheetPanel)
+		writeMappingDefaults()
 	}
 	
 	@IBAction open func addMappingCancelSheet(_ aSender: Any?) {
-		
+		window.endSheet(addSheetPanel)
 	}
 	
-	@IBAction open func choosePreset(_ aSender: Any?) {
-		
+	@IBAction open func choosePreset(_ aSender: NSPopUpButton?) {
+		guard let preset = aSender?.selectedItem?.representedObject as? [String: Any] else {
+			return
+		}
+		addLocalPortField.objectValue = preset["localPort"]
+		addDesiredField.objectValue = preset["desiredPort"]
+		addReferenceStringField.objectValue = preset["referenceString"]
+		addDescriptionField.objectValue = preset["mappingTile"]
+		if let transportProto = preset["transportProtocol"] as? Int {
+			let proto2 = TCMPortMappingTransportProtocol(rawValue: UInt8(transportProto))
+			addProtocolTCPButton.state = proto2.contains(.TCP) ? .on : .off
+			addProtocolUDPButton.state = proto2.contains(.UDP) ? .on : .off
+		}
 	}
 	
 	@IBAction open func showInstructionalPanel(_ aSender: Any?) {
-		
+		if !UserDefaults.standard.bool(forKey: "DontShowInstructionsAgain") {
+			window.beginSheet(instructionalSheetPanel) { (retVal) in
+				self.instructionalSheetPanel.orderOut(self)
+			}
+		}
 	}
 	
-	@IBAction open func endInstructionalSheet(_ aSender: Any?) {
+	@IBAction open func endInstructionalSheet(_ aSender: NSButton?) {
+		if aSender?.tag == 42 {
+			if let theURL = URL(string: NSLocalizedString("NAT-PMP Howto URL", comment: "")) {
+				NSWorkspace.shared.open(theURL)
+			} else {
+				NSSound.beep()
+			}
+		}
 		
+		if dontShowInstructionsAgainButton.state == .on {
+			UserDefaults.standard.set(true, forKey: "DontShowInstructionsAgain")
+		}
+
+		window.endSheet(instructionalSheetPanel)
 	}
 	
 	
@@ -282,25 +344,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 	
 	@IBAction open func requestUPNPMappingTable(_ aSender: Any?) {
-		
+		progressIndictatorTabItem.tabView?.selectTabViewItem(progressIndictatorTabItem)
+		TCMPortMapper.shared.requestUPNPMappingTable()
+		window.beginSheet(showUPNPMappingListWindow) { (resp) in
+			self.showUPNPMappingListWindow.orderOut(self)
+		}
 	}
 	
 	@objc func portMapperDidReceiveUPNPMappingTable(_ aNotification: Notification) {
-		
+		UPNPMappingListArrayController.content = aNotification.userInfo?["mappingTable"]
+		upnpMappingListTabItem.tabView?.selectTabViewItem(upnpMappingListTabItem)
 	}
 
 	@IBAction open func requestUPNPMappingTableRemoveMappings(_ aSender: Any?) {
-		
+		TCMPortMapper.shared.removeUPNPMappings(UPNPMappingListArrayController.selectedObjects as! [[String : Any]])
+		window.endSheet(showUPNPMappingListWindow)
 	}
 	
 	@IBAction open func requestUPNPMappingTableOKSheet(_ aSender: Any?) {
-		
+		window.endSheet(showUPNPMappingListWindow)
 	}
 	
 	override func controlTextDidChange(_ obj: Notification) {
-		
+		if let fieldEditor = obj.userInfo?["NSFieldEditor"] as? NSTextView {
+			if fieldEditor === addLocalPortField.currentEditor() {
+				addDesiredField.stringValue = addLocalPortField.stringValue
+			}
+		}
+		let validPortRange = 0..<Int(UInt16.max)
+		invalidLocalPortView.isHidden = !validPortRange.contains(addLocalPortField.integerValue)
+		invalidDesiredPortView.isHidden = !validPortRange.contains(addDesiredField.integerValue)
 	}
-	
-	//- (void)controlTextDidChange:(NSNotification *)aNotification
-	
 }
